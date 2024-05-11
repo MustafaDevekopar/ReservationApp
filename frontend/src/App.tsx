098684e794c186d86b6a8f6a2d9e88b218ef65e0
mustafa <mustafadevelopar@gmail.com>
2024-05-11 22:02:51 +0300

    Show navbar and navbarIcon only in some pages py using useLocation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,30 @@ import './App.css';
 import NavBarIcons from './Components/Navbar/NavBarIcons';
 import NavBarIconsMobile from './Components/Navbar/NavBarIconsMobile';
 import Navbar from './Components/Navbar/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function App() {
+  // Get the current location using react-router's useLocation hook
+  const location = useLocation();
+
+  // Define an array of paths where I want to show navbar and navbar Icon 
+  const pathsToHideComponents = ['/', '/favorite', "/search", "/posts"];
+
+  // Check if the current path is in the pathsToShowArray array
+  const shouldHideComponent = pathsToHideComponents.includes(location.pathname);
+
   return (
     <div className="App">
-      <Navbar />
-      <NavBarIcons />
-      <NavBarIconsMobile />
-      <Outlet />
-      {/* <HomePage /> */}
-      {/* <FavoritePage /> */}
-      {/* <SearchPage /> */}
+      {/* Conditional rendering of Component to show */}
+      {shouldHideComponent && <Navbar />}
+      {shouldHideComponent && <NavBarIconsMobile />}
+      {shouldHideComponent && <NavBarIcons />}
 
+      <Outlet />
     </div>
   );
 }
 
 export default App;
+
+
