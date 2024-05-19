1f8e4789fc8eb401706727d35962d1cc92cf722b
mustafa <mustafadevelopar@gmail.com>
2024-05-19 16:35:52 +0300

    backend .. add FootballFieldupdateDto

---
+++ Reservations/Dto/FootballfieldUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Reservations.Dto
+{
+    public class FootballfieldUpdateDto
+    {
+        public int categoryId { get; set; }
+        public int governorateId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public decimal PhoneNumbr { get; set; }
+        public string Location { get; set; }
+        public IFormFile? Avatar { get; set; }
+    }
+}
